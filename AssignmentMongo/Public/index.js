f806d370a34569f3a8c211d08b60f8a384a23f1f
Chelsa on Mac <cmae23@hotmail.com>
2014-02-12 18:40:00 -0800

    adding simple MongoAssignment test

---
+++ AssignmentMongo/Public/index.js
@@ -0,0 +1,14 @@
+/**
+ * @author Chelsa Hiatt
+ */
+
+
+$(document).ready(function() {
+
+	$.getJSON('/read', function(data) {
+		console.log(data);
+		for (var i = 0; i < data.length; i++) {
+			$("#result").append('<li>' + JSON.stringify(data[i]) + '</li>');
+		}
+	});
+});
