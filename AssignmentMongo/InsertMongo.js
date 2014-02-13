70973397dc9fa78ec15be9faed134e19b4fe4887
Chelsa on Mac <cmae23@hotmail.com>
2014-02-12 20:22:59 -0800

    added insert of 250 objects and read to html file

--- AssignmentMongo/InsertMongo.js
+++ AssignmentMongo/InsertMongo.js
@@ -4,15 +4,31 @@ var format = require('util').format;
 var url01 = 'mongodb://127.0.0.1:27017/test';
 var url02 = 'mongodb://192.168.2.19:27017/test';
 
-  MongoClient.connect(url01, function(err, db) {
-    if(err) throw err;
+  MongoClient.connect(url01, function(err, db) {'use strict';
+    if(err) {throw err;}
     
     var collection = db.collection('test_data');
     
-    // insert into collection  <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<< UPDATE WITH LOOP AFTER WORKING
-    collection.insert({"firstName": "John", "lastName": "Adams"}, function(err, docs) {
-
-      collection.count(function(err, count) {
+    // array to hold collection objects to insert
+    var dataArray = [];
+    
+    // populate the array with address fields
+    for (var i = 10000; i < 10251; i = i + 1) {
+		var person = {
+			firstName : 'Rita' + i,
+			lastName : 'Hill' + i,
+			address : i + " " + "Ruby Street",
+			city : 'Bellevue',
+			state : 'WA',
+			zip : '98002'
+		};
+		// add the new person to the array
+		dataArray.push(person);
+		}
+		
+    // insert array into collection
+    collection.insert(dataArray, function(err, docs) {
+      	collection.count(function(err, count) {
         console.log(format("count = %s", count));
       });
 
--- AssignmentMongo/Public/index.js
+++ AssignmentMongo/Public/index.js
@@ -5,6 +5,28 @@
 
 $(document).ready(function() {
 
+	// displays one record in the application
+	$.getJSON('/read', function(data) {
+		console.log(data[1]);
+		console.log("The first name is: "+ data[1].firstName);
+		
+				//$('#Div01').load("source/source.html #paragraph01", function() {
+				//console.log("Load source01 was performed");
+				//});
+				
+		// load to html
+		$('#name').load('<p>'+data[1].firstName + ' ' + data[1].lastName+'<p>', function(){
+			console.log("Name was loaded for object " + data.valueOf);
+		});
+		$('#address').load('<p>'+data[1].address+'<p>', function(){
+			console.log("Address was loaded for object " + data.valueOf);
+		});
+		$("#cityStateZip").load('<p>'+data[1].city + ', '+data[1].state + ' ' + data[1].zip+'<p>', function(){
+			console.log("City, State, Zip was loaded for object " + data.valueOf);
+		});
+		
+	});
+
 	$.getJSON('/read', function(data) {
 		console.log(data);
 		for (var i = 0; i < data.length; i++) {
