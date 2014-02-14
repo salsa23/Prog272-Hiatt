3e8f365494924a851ae8329cd7be6935b2e2e693
Chelsa on Mac <cmae23@hotmail.com>
2014-02-14 00:11:12 -0800

    update to button parameter function

--- Week05-NodeExpressMongo-Hiatt/Public/index.js
+++ Week05-NodeExpressMongo-Hiatt/Public/index.js
@@ -1,38 +1,37 @@
 /**
  * @author Chelsa Hiatt
  */
-function showData(){ 
-	var i = $('#userChoice').val();
-	
+function showData() {
+	//var i = $('#userChoice').val();
+
 	// button to select record to display
-	$('#submit').click({i:$('#userChoice').val()},getRecord);
-	
-	//$("submit").click(function(){
-	//get user value
-	//	var i= $("userChoice").val();	
-		// call function with parameter
-	//	var result = getRecord(i);
-	//});
-	
-	// get the selected record	
-	function getRecord(event){
-		var i = event.data.i;
-		// load to html
-		$('#name').html('<p>'+data[i-1].firstName + ' ' + data[i-1].lastName+'<p>', function(){
-			console.log("Name was loaded for object");
-		});
-		$('#address').html('<p>'+data[i-1].address+'<p>', function(){
-			console.log("Address was loaded for object");
-		});
-		$("#cityStateZip").html('<p>'+data[i-1].city + ', '+data[i-1].state + ' ' + data[i-1].zip+'<p>', function(){
-			console.log("City, State, Zip was loaded for object ");
+	$('#submit').click({x : $('#userChoice').val()}, getRecord);
+
+	// get the selected record
+	function getRecord(event) {
+		$.getJSON('/read', function(data) {
+			// set x to x-1 to accommodate array numbering and user non-use of 0 when requesting the first record.
+			var x = (event.data.x - 1);
+			if (x > 0 && x < data.length){
+				// load to html
+				$('#name').html('<p>' + data[x].firstName + ' ' + data[x].lastName + '<p>', function() {
+					console.log("Name was loaded for object");
+				});
+				$('#address').html('<p>' + data[x].address + '<p>', function() {
+					console.log("Address was loaded for object");
+				});
+				$("#cityStateZip").html('<p>' + data[x].city + ', ' + data[x].state + ' ' + data[x].zip + '<p>', function() {
+					console.log("City, State, Zip was loaded for object ");
+				});
+			}else{
+				$('#error').html("That record number does not exist. Please enter a different number.");	
+			};
 		});
 	};
-	
-	
+
 	// displays one record in the html page from the returned data <<<<<<<<<<<<<<<<<<<<< Remove to test
 	//$.getJSON('/read', function(data) {
-		// load to html
+	// load to html
 	//	$('#name').html('<p>'+data[1].firstName + ' ' + data[1].lastName+'<p>', function(){
 	//		console.log("Name was loaded for object");
 	//	});
@@ -41,7 +40,7 @@ function showData(){
 	//	});
 	//	$("#cityStateZip").html('<p>'+data[1].city + ', '+data[1].state + ' ' + data[1].zip+'<p>', function(){
 	//		console.log("City, State, Zip was loaded for object ");
-	//	});	
+	//	});
 	//});
 
 	$.getJSON('/read', function(data) {
@@ -50,7 +49,7 @@ function showData(){
 			$("#results").append('<li>' + JSON.stringify(data[i]) + '</li>');
 		}
 	});
-	
+
 };
 
 $(document).ready(function() {
