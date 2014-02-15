b324cc41ad346048ddd7ee5fb71bc0ed0c224da3
Chelsa on Mac <cmae23@hotmail.com>
2014-02-15 13:27:48 -0800

    renamed some files, updated server - added app.use

--- NodeModules_Hiatt/server.js
+++ NodeModules_Hiatt/server.js
@@ -1,19 +1,21 @@
 /**
- * @author Salsa
+ * @author Chelsa Hiatt
  */
 
 // Link in Modules
-var mod01 = require('Module01');
-var mod02 = require('./Library/Module02');
-var mod03 = require('./Public/Module03');
+var getCalc = require('./getCalcMod');
+var getMiles = require('./Library/getMilesMod');
+var getSqRoot = require('./Public/getSqRootMod');
+
+//app.use("/", express.static(__dirname + '/'));
 
 // using exported methods from Mod01
-console.log("Adding 4+3 returns: " + mod01.add(4,3));
-console.log("Multiplying 4*3 returns: "+ mod01.multiply(4,3));
-console.log("Subtracting 4-3 returns: "+ mod01.subtract(4,3));
+console.log("Adding 4+3 returns: " + getCalc.add(4,3));
+console.log("Multiplying 4*3 returns: "+ getCalc.multiply(4,3));
+console.log("Subtracting 4-3 returns: "+ getCalc.subtract(4,3));
 
 // using exported method from Mod02
-console.log("50 feet in miles is: " + mod02.feetToMiles(50));
+console.log("50 feet in miles is: " + getMiles.feetToMiles(50));
 
 // using modular pattern from Mod03, passing in an object with a num value
-mod03.myObject.squareRoot({num:16});
+getSqRoot.myObject.squareRoot({num:16});
