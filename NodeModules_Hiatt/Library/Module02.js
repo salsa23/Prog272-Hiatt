f01211bad789c8e277913d480d6c1f37e9c8c1e9
Chelsa <cmae23@hotmail.com>
2014-02-14 16:42:03 -0800

    update value of feet in one mile

--- NodeModules_Hiatt/Library/Module02.js
+++ NodeModules_Hiatt/Library/Module02.js
@@ -4,7 +4,7 @@
  * Module02 exports a method that will convert feetToMiles.
  */
 function feetToMiles(f) {
-	var feetInOneMile = 50;
+	var feetInOneMile = 5280;
 	var feet = f;
 	var retVal = 0;
 	if (feet>0)
