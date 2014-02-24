0fd56c863b12e0fb78f9a9cc7d22a6c6822c0857
Chelsa on Mac <cmae23@hotmail.com>
2014-02-24 09:54:04 -0800

    adding search functionality

--- Week07-Midterm-Hiatt/MidtermWeb_Hiatt/Public/index.js
+++ Week07-Midterm-Hiatt/MidtermWeb_Hiatt/Public/index.js
@@ -3,17 +3,15 @@ var MongoData = (function() { 'use strict';
 	var mongoData = null;
 
 	function MongoData() {		
-		//$("#readAll").click(queryAll);
 		//$("#readTwo").click(queryTwo);
-
 		
 		$("#uploadCollection").click(loadCollection);
 		$("#deleteCollection").click(removeCollection);	
 		//$("#backupCollection").click(backupCollection);	// MAKE METHOD backupCollection --> write to JSON file
 
-		$("#getTitles").click(queryAll);
-		$("#poemContents").click(poemContents);
-		$("#searchKeywords").click(searchKeywords);
+		$("#getTitles").click(getTitles);			// uses queryAll to populate mongoData, then adds titles
+		$("#poemContents").click(poemContents);		// uses local mongoData
+		$("#searchKeywords").click(searchKeywords);	// uses local mongoData
 		$("#addPoem").click(addPoem);
 		$("#deletePoem").click(deletePoem);
 		$("#showData").click(showData);				// calls function to display one record that user specifies
@@ -21,6 +19,7 @@ var MongoData = (function() { 'use strict';
 
 	// displays one record from the database as index identifies
 	var displayRecord = function(index) {
+		console.log("displayRecord called");
 		$('#dataDisplay').empty();
 		var poem = 
 			"<h2>Title: " + mongoData[index].title + "</h2>"+
@@ -28,29 +27,17 @@ var MongoData = (function() { 'use strict';
 			"<hr/>"+
 			"<p>" + mongoData[index].content + "<p>";
 		$('#dataDisplay').html(poem);
-		
-		//$('#firstName').html(mongoData[index].firstName);
-		//$('#lastName').html(mongoData[index].lastName);
-		//$('#address').html(mongoData[index].address);
-		//$('#city').html(mongoData[index].city);
-		//$('#state').html(mongoData[index].state);
-		//$('#zip').html(mongoData[index].zip);
 	};
 
 	// displays one record as selected by user
 	var poemContents = function() {
 		console.log("poemContents called");
-		var index = $("select.#poemTitles").val();
+		var index = $("#poemTitles").val();
 		console.log("index: " + index);
-		
-		if(index>=0){
-			displayRecord(index);
-		}else{
-			alert("That is not a valid record ID.");
-		}
+		displayRecord(index);
 	};
 	
-	// displays one record as selected by user -- see poemContents
+	// MAY NOT USE --- displays one record as selected by user -- see poemContents
 	var showData = function() {
 		console.log("showData called");
 		var index = $("select.#poemTitles").val();
@@ -66,7 +53,8 @@ var MongoData = (function() { 'use strict';
 	// displays titles in dropdown, setting value to ID
 	var getTitles = function() {
 		console.log("getTitles called");
-		//$("#mongoData").empty();
+		queryAll();				// refreshes the mongoData
+		
 		$("#poemTitles").empty();
 		for (var i=0; i< mongoData.length; i=i+1){
 			//$("#mongoData").append('<li>'+mongoData[i].title + '<li>');
@@ -77,6 +65,44 @@ var MongoData = (function() { 'use strict';
 		console.log("Titles Loaded");
 	};
 	
+	// displays titles in list
+	var displayTitleList = function(keyArray) {
+		console.log("displayTitleList called");
+		
+		$("#dataDisplay").empty();
+		var keyTitles = '<ul>';
+		for (var i=0; i< keyArray.length; i=i+1){
+			keyTitles = keyTitles+ '<li>' + keyArray[i].title + '<li>';
+		}
+		keyTitles = keyTitles + '</ul>';
+		console.log("keyTitle html: "+ keyTitles);
+		console.log("TitleList Loaded");
+	};
+	
+	// searches records that hold keywords as selected by user
+	var searchKeywords = function() {
+		console.log("searchKeywords called");
+		queryAll();				// refreshes the mongoData
+		
+		var keyword = $("#keywords").val();
+		console.log("User Keyword: " + keyword);
+		var keyArray = [];
+		for(var i=0; i<mongoData.length; i+i+1){
+			// search each records keyword array within
+			for(var j=0;j<mongoData[i].keywords.length; j=j+1){
+				console.log("inside keyword array, the keywords are: " + mongoData[i].keywords);
+				if(mongoData[i].keywords[j] === keyword){
+					console.log("found the keyword: " + keyword);
+					keyArray.push(mongoData[i]);
+					console.log("pushed object into array: " + mongoData[i]);
+				}
+			}
+		}
+		console.log("keyArray: "+ keyArray);
+		displayTitleList(keyArray);
+		console.log("keyword search complete");
+	};
+	
 	// displays database poem title elements in HTML buttons
 	var queryAll = function() {
 		$.getJSON('/readAll', function(data) {
@@ -84,8 +110,6 @@ var MongoData = (function() { 'use strict';
 			mongoData = data;
 			console.log("mongoData in queryAll: " + mongoData);
 			//console.log("Data in queryAll: " + data);
-			
-			getTitles();		// populates poem dropdown list with ID and Title
 			
 			//displayRecord(0);
 			// displays list of database entries in HTML div #mongoData  <<<<<<<<<<<<<<<<<<<< POSSIBLY REMOVE IF NOT USING <<<<<
