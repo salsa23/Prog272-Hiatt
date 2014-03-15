a17b75862ab1dd4d34d5cf10c0f0cfae008eadea
ChelsaVM <cmae23@hotmail.com>
2014-03-15 18:08:18 -0400

    adding Week10 Final set up files

---
+++ Week10Setup/StaticSite/NodeStatic.js
@@ -0,0 +1,16 @@
+var static = require('node-static');
+
+/*
+ * Create a node-static server instance to serve the './Site' folder
+ * You'll need to run node install or npm install node-static
+ * See: https://github.com/cloudhead/node-static
+ */
+var file = new static.Server('./Site');
+var port = 40025;
+
+require('http').createServer(function (request, response) {
+    request.addListener('end', function () {
+        file.serve(request, response);
+    }).resume();	
+}).listen(port);
+console.log("Listing on port:", port);
